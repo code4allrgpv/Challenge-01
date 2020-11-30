337bfe6adf3bbeca2d9a24a8be03efd690def86b
Code4All <75253091+code4allrgpv@users.noreply.github.com>
2020-11-30 23:06:08 +0530

    Second Commit c4a01, Shell Update Busted

--- Codaemon's Divisor Sum Differences.cpp
+++ Codaemon's Divisor Sum Differences.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Let n = 6,
+// => F(1) + F(2) + F(3) + F(4) + F(5) + F(6)
+// => 1 will occurs 6 times in F(1), F(2),
+//    F(3), F(4), F(5) and F(6)
+// => 2 will occurs 3 times in F(2), F(4) and
+//    F(6)
+// => 3 will occur 2 times in F(3) and F(6)
+// => 4 will occur 1 times in F(4)
+// => 5 will occur 1 times in F(5)
+// => 6 will occur 1 times in F(6)
+
 int main() {
     long long int a, b, ans = 0;
     cin >> a >> b;
